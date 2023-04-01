13ea97a8f00127809b177954e6d71bcf611dd4b7
jiamubai <76642435+jiamubai@users.noreply.github.com>
2023-04-01 04:13:24 -0400

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -2,10 +2,24 @@ from tqdm import tqdm
 
 
 file = open('order_products__prior.csv', 'r')
+file = open('order_products__train.csv', 'r')
 
 order_id = -99
 items = []
 with open("train.txt", 'w') as f:
+    for lines in tqdm(file):
+        lines = lines.strip()
+        lines = lines.split(',')
+        if order_id != lines[0]:
+            if len(items) > 1:
+                f.write('%s:%s\n' % (order_id, items))
+            order_id = lines[0]
+            items = []
+        items.append(lines[1])
+    f.write('%s:%s\n' % (order_id, items))
+    
+items = []
+with open("eval.txt", 'w') as f:
     for lines in tqdm(file):
         lines = lines.strip()
         lines = lines.split(',')
